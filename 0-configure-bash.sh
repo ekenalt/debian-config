2b8a4cf871fc17e2fcab9798039e6c13d3553ba7
ekenalt <ekenalt@gmail.com>
2023-07-24 10:24:16 -0500

    fixed script name and sources

--- 0-configure-bash.sh
+++ 0-configure-bash.sh
@@ -2,9 +2,9 @@
 
 sudo apt update
 sudo apt install git wget curl zsh build-essential neovim extrepo -y
-read -p 'git user.name:' gitusername
+read -p 'git user.name: ' gitusername
 git config --global user.name $gitusername
-read -p 'git user.email:' gituseremail
+read -p 'git user.email: ' gituseremail
 git config --global user.email $gituseremail
 git config --global core.autocrlf input
-zsh ./shellconfig1.sh
+zsh ./1-configure-zsh.sh
