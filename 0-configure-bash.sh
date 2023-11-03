365704cd218643df8cf716cc96333a00ea37f62e
ekenalt <ekenalt@gmail.com>
2023-11-03 09:53:29 -0500

    wsl support cleanup

--- 0-configure-bash.sh
+++ 0-configure-bash.sh
@@ -11,4 +11,5 @@ git config --global user.name $gitusername
 read -p 'git user.email: ' gituseremail
 git config --global user.email $gituseremail
 git config --global core.autocrlf input
+git config --global core.editor 'vim'
 zsh ./1-configure-zsh.sh
--- 1-configure-zsh.sh
+++ 1-configure-zsh.sh
@@ -5,16 +5,16 @@ git clone --recursive https://github.com/ekenalt/prezto.git "${ZDOTDIR:-$HOME}/.
 setopt EXTENDED_GLOB
 
 ZSHRC_PATH="${ZDOTDIR:-$HOME}"/.zprezto/runcoms/zshrc
-SOURCE_ZSH_WSL_SED_ARGUMENT='s|#SOURCE_ZSH_WSL||g'
+SOURCE_ZSH_WSL_SED_ARGUMENT='s|# Source .zsh_wsl||g'
 
 if test -f /proc/sys/fs/binfmt_misc/WSLInterop
 then
-  SOURCE_ZSH_WSL_SED_ARGUMENT='s|#SOURCE_ZSH_WSL|\n. ~/.zsh_wsl|g'
+  SOURCE_ZSH_WSL_SED_ARGUMENT='s|# Source .zsh_wsl|# Source .zsh_wsl\nif [[ -s "${ZDOTDIR:-$HOME}/.zsh_wsl" ]]; then\n  source "${ZDOTDIR:-$HOME}/.zsh_wsl"\nfi|g'
 else
   \rm -f "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/zsh_wsl
 fi
 
-sed -e "${SOURCE_ZSH_WSL_SED_ARGUMENT}" "${ZSHRC_PATH}" | tee "${ZSHRC_PATH}.new"
+sed -e "${SOURCE_ZSH_WSL_SED_ARGUMENT}" "${ZSHRC_PATH}" | cat -s | tee "${ZSHRC_PATH}.new"
 mv -f "${ZSHRC_PATH}.new" "${ZSHRC_PATH}"
 
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
