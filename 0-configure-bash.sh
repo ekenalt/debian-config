91f9bd7a769395979700f2fa7b82069658780839
ekenalt <ekenalt@gmail.com>
2023-11-23 01:30:18 -0600

    changed git core.pager

--- 0-configure-bash.sh
+++ 0-configure-bash.sh
@@ -12,4 +12,5 @@ read -p 'git user.email: ' gituseremail
 git config --global user.email $gituseremail
 git config --global core.autocrlf input
 git config --global core.editor 'vi'
+git config --global core.pager 'less -FX'
 zsh ./1-configure-zsh.sh
